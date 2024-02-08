8ecf11c1f187de0cdd6f1fba1e4153217f884aa2
Jdob11 <jbdobson11@gmail.com>
2024-02-08 15:44:56 -0800

    removed commented code

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-
-// Create a new task by adding to the arrays
-// A new task will be created as incomplete
 function newTask(title, description) {
   const task = {
     title: title,
@@ -19,25 +16,13 @@ function newTask(title, description) {
   return task;
 };
 
-// // prints out the provided task's details
-// function logTaskState(task) {
-//   console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
-// }
-
-// // marks the provided task as completed
-// function completeTask(task) {
-//   task.complete = true;
-// }
-
-// DRIVER CODE BELOW
-
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
 
-task1.logTaskState(); // Clean Cat Litter has not been completed
+task1.logTaskState();
 task1.markCompleted();
-task1.logTaskState(); // Clean Cat Litter has been completed
+task1.logTaskState();
 
 console.log(tasks);
 
